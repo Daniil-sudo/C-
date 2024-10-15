9a0aa56672c8bd41e6d3ae3b78e75e3fd95b4e49
Daniil-sudo <daniilhodnevich@gmail.com>
2024-10-15 16:17:25 +0300

    Add files via upload
    Программа, которая тренирует твою память выдавая случайные числа и скрывая их.

---
+++ тренируй память 1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    while (true) {
+        int randnamber = rand() % 100 + 1; // Исправленное объявление
+        int user;
+
+        cout << "remember number: ";
+        cout << randnamber << endl; // Добавлено endl для переноса строки
+        Sleep(1500); // Пауза на 1.5 
+        system("cls"); // чистка консоли
+        cout << "insert: ";
+
+        cin >> user;
+
+        if (user == randnamber) {
+            cout << "true!" << endl;
+        }
+        else {
+            cout << "err: " << randnamber << endl; // Исправленное else
+        }
+        cout << "resume" << endl;
+        char resume;
+        cin >> resume;
+
+            if (resume == '\r')
+            {
+                break;
+            }
+
+       
+    }
+    cout << "bye!" << endl;
+    return 0;
+}
